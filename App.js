d77baf5a1ae65c993886c9abb431359f569bfd85
Jacob Bernstein <jacobjbernstein@gmail.com>
2018-12-17 09:38:53 +0900

    Adding places to list

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
  //command D to enable auto reload
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -21,7 +21,8 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
 
   placeNameChangedHandler = val => {
@@ -30,15 +31,39 @@ export default class App extends Component<Props> {
     })
   }
 
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === ""){
+      return
+    }
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      }
+    })
+  }
+
   render() {
+    const placesOutput = this.state.places.map(place => (
+      <Text>{place}</Text>
+    ))
     return (
       <View style={styles.container}>
-        <TextInput
-          style = {{width: 100}}
-          value = {this.state.placeName}
-          onChangeText = {this.placeNameChangedHandler}
-          placeholder = "Joe"
-        /> 
+        <View style={styles.inputContainer}>
+          <TextInput
+            style = {styles.placeInput}
+            value = {this.state.placeName}
+            onChangeText = {this.placeNameChangedHandler}
+            placeholder = "An Awesome Place"
+          /> 
+          <Button
+            title = "Add"
+            style = {styles.placeButton}
+            onPress = {this.placeSubmitHandler}
+          />
+        </View>
+        <View>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -46,10 +71,24 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    flex: 1, //this means container takes whole page
+    padding: 100,
+    justifyContent: 'flex-start', //start element at beginning of page
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  inputContainer: {
+    //flex: 1, // don't need inner container to take full height of page
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
   },
   welcome: {
     fontSize: 20,
