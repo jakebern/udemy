0b76e41d483609c074baf8fb2a45a59f3df2a434
Jacob Bernstein <jacobjbernstein@gmail.com>
2018-12-26 16:19:54 +0900

    Adding Signup Mode and Connecting Redux

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -7,6 +7,7 @@ import {
 	ImageBackground,
 	Dimensions
 } from "react-native";
+import { connect } from "react-redux";
 import startMainTabs from "../MainTabs/startMainTabs";
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 import HeadingText from "../../components/UI/HeadingText/HeadingText";
@@ -14,12 +15,14 @@ import MainText from "../../components/UI/MainText/MainText";
 import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
 import backgroundImage from "../../assets/backgroundpic.jpg";
 import validate from "../../utility/validation";
+import { tryAuth } from "../../store/actions/index";
 
 class AuthScreen extends Component {
 	//need state because want state to listen for dimension changes
 	//and for UI to update once dimension changes
 	state = {
 		viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
+		authMode: "login",
 		//keep controls in this component vs. redux
 		//because state is internal to this component
 		controls: {
@@ -60,12 +63,25 @@ class AuthScreen extends Component {
 		Dimensions.removeEventListener("change", this.updateStyles);
 	}
 
+	switchAuthModeHandler = () => {
+		this.setState(prevState => {
+			return {
+				authMode: prevState.authMode === "login" ? "signup" : "login"
+			};
+		});
+	};
+
 	updateStyles = dims => {
 		this.setState({
 			viewMode: dims.window.height > 500 ? "portrait" : "landscape"
 		});
 	};
 	loginHandler = () => {
+		const authData = {
+			email: this.state.controls.email.value,
+			password: this.state.controls.password.value
+		};
+		this.props.onLogin(authData);
 		startMainTabs();
 	};
 
@@ -124,6 +140,7 @@ class AuthScreen extends Component {
 	};
 	render() {
 		let headingText = null;
+		let confirmPasswordControl = null;
 		//only show heading text if have lots of vert space
 		if (this.state.viewMode === "portrait") {
 			headingText = (
@@ -132,12 +149,35 @@ class AuthScreen extends Component {
 				</MainText>
 			);
 		}
+		if (this.state.authMode === "signup") {
+			confirmPasswordControl = (
+				<View
+					style={
+						this.state.viewMode === "portrait"
+							? styles.portraitPasswordWrapper
+							: styles.landscapePasswordWrapper
+					}
+				>
+					<DefaultInput
+						placeholder="Confirm Password"
+						style={styles.input}
+						value={this.state.controls.confirmPassword.value}
+						onChangeText={val => this.updateInputState("confirmPassword", val)}
+						valid={this.state.controls.confirmPassword.valid}
+						touched={this.state.controls.confirmPassword.touched}
+					/>
+				</View>
+			);
+		}
 		return (
 			<ImageBackground source={backgroundImage} style={styles.backgroundImage}>
 				<View style={styles.container}>
 					{headingText}
-					<ButtonWithBackground color="#29aaf4" onPress={() => alert("hello")}>
-						Switch to Login
+					<ButtonWithBackground
+						color="#29aaf4"
+						onPress={this.switchAuthModeHandler}
+					>
+						Switch to {this.state.authMode === "login" ? "Sign Up!" : "Login"}
 					</ButtonWithBackground>
 					<View style={styles.inputContainer}>
 						<DefaultInput
@@ -150,14 +190,16 @@ class AuthScreen extends Component {
 						/>
 						<View
 							style={
-								this.state.viewMode === "portrait"
+								this.state.viewMode === "portrait" ||
+								this.state.authMode === "login"
 									? styles.portraitPasswordContainer
 									: styles.landscapePasswordContainer
 							}
 						>
 							<View
 								style={
-									this.state.viewMode === "portrait"
+									this.state.viewMode === "portrait" ||
+									this.state.authMode === "login"
 										? styles.portraitPasswordWrapper
 										: styles.landscapePasswordWrapper
 								}
@@ -171,31 +213,15 @@ class AuthScreen extends Component {
 									touched={this.state.controls.password.touched}
 								/>
 							</View>
-							<View
-								style={
-									this.state.viewMode === "portrait"
-										? styles.portraitPasswordWrapper
-										: styles.landscapePasswordWrapper
-								}
-							>
-								<DefaultInput
-									placeholder="Confirm Password"
-									style={styles.input}
-									value={this.state.controls.confirmPassword.value}
-									onChangeText={val =>
-										this.updateInputState("confirmPassword", val)
-									}
-									valid={this.state.controls.confirmPassword.valid}
-									touched={this.state.controls.confirmPassword.touched}
-								/>
-							</View>
+							{confirmPasswordControl}
 						</View>
 					</View>
 					<ButtonWithBackground
 						color="#29aaf4"
 						onPress={this.loginHandler}
 						disabled={
-							!this.state.controls.confirmPassword.valid ||
+							(!this.state.controls.confirmPassword.valid &&
+								this.state.authMode === "signup") ||
 							!this.state.controls.email.valid ||
 							!this.state.controls.password.valid
 						}
@@ -244,4 +270,13 @@ const styles = StyleSheet.create({
 	}
 });
 
-export default AuthScreen;
+const mapDispatchToProps = dispatch => {
+	return {
+		onLogin: authData => dispatch(tryAuth(authData))
+	};
+};
+
+export default connect(
+	null,
+	mapDispatchToProps
+)(AuthScreen);
