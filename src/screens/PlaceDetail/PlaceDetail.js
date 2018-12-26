e6733bcb587028c24ca5ec7cbee37c91776fc1ba
Jacob Bernstein <jacobjbernstein@gmail.com>
2018-12-26 09:38:23 +0900

    Responsive Place Details

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -5,13 +5,35 @@ import {
 	Text,
 	StyleSheet,
 	TouchableOpacity,
-	Platform
+	Platform,
+	Dimensions
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { connect } from "react-redux";
 import { deletePlace } from "../../store/actions/index";
 
 class placeDetail extends Component {
+	//just good for the initial state
+	state = {
+		viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+	};
+
+	constructor(props) {
+		super(props);
+		Dimensions.addEventListener("change", this.updateStyles);
+	}
+
+	//remove event listener when compenent is not active
+	componentWillUnmount() {
+		Dimensions.removeEventListener("change", this.updateStyles);
+	}
+
+	updateStyles = dims => {
+		this.setState({
+			viewMode: dims.window.height > 500 ? "portrait" : "landscape"
+		});
+	};
+
 	placeDeleteHandler = () => {
 		this.props.onDeletePlace(this.props.selectedPlace.key);
 		this.props.navigator.pop();
@@ -19,27 +41,46 @@ class placeDetail extends Component {
 
 	render() {
 		return (
-			<View style={styles.container}>
-				<Image
-					style={styles.placeImage}
-					source={this.props.selectedPlace.image}
-				/>
-				<Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
-				<View>
-					{/* 
-						Know you need props here because outer word needs to know
-						about if button is pressed.  Thus pass function in via parent
-						so parent can know about the press
-					*/}
-					<TouchableOpacity onPress={this.placeDeleteHandler}>
-						<View style={styles.deleteButton}>
-							<Icon
-								size={30}
-								name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
-								color="red"
-							/>
-						</View>
-					</TouchableOpacity>
+			<View
+				style={[
+					styles.container,
+					this.state.viewMode === "portrait"
+						? styles.portraitContainer
+						: styles.landscapeContainer
+				]}
+			>
+				{/*split into two high level views
+		 because in Lanscape want 2 column view
+		 with image on left, text/button on right*/}
+
+				{/*need subcontainer to set equal priority 
+	to each subcontainer so that way each takes 50% of 
+horizontal space*/}
+				<View style={styles.subContainer}>
+					<Image
+						style={styles.placeImage}
+						source={this.props.selectedPlace.image}
+					/>
+				</View>
+
+				<View style={styles.subContainer}>
+					<View>
+						<Text style={styles.placeName}>
+							{this.props.selectedPlace.name}
+						</Text>
+					</View>
+
+					<View>
+						<TouchableOpacity onPress={this.placeDeleteHandler}>
+							<View style={styles.deleteButton}>
+								<Icon
+									size={30}
+									name={Platform.OS === "android" ? "md-trash" : "ios-trash"}
+									color="red"
+								/>
+							</View>
+						</TouchableOpacity>
+					</View>
 				</View>
 			</View>
 		);
@@ -48,7 +89,17 @@ class placeDetail extends Component {
 
 const styles = StyleSheet.create({
 	container: {
+		flex: 1,
 		margin: 50
+	},
+	subContainer: {
+		flex: 1
+	},
+	portraitContainer: {
+		flexDirection: "column"
+	},
+	landscapeContainer: {
+		flexDirection: "row"
 	},
 	placeImage: {
 		width: "100%",
