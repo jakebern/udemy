28543b3eebc0173990d0db9f7c21d9ff769b0a5d
Jacob Bernstein <jacobjbernstein@gmail.com>
2018-12-26 20:54:10 +0900

    Added Marker

--- src/components/LocationSelector/LocationSelector.js
+++ src/components/LocationSelector/LocationSelector.js
@@ -11,15 +11,39 @@ class locationSelector extends Component {
 			longitudeDelta:
 				(Dimensions.get("window").width / Dimensions.get("window").height) *
 				0.0122
-		}
+		},
+		locationChosen: false
 	};
+
+	pickLocationHandler = event => {
+		const coords = event.nativeEvent.coordinate;
+		this.setState(prevState => {
+			return {
+				focusedLocation: {
+					...prevState.focusedLocation,
+					latitude: coords.latitude,
+					longitude: coords.longitude
+				},
+				locationChosen: true
+			};
+		});
+	};
+
 	render() {
+		let marker = null;
+		if (this.state.locationChosen) {
+			marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+		}
 		return (
 			<View style={styles.container}>
 				<MapView
 					style={styles.map}
 					initialRegion={this.state.focusedLocation}
-				/>
+					region={this.state.focusedLocation}
+					onPress={this.pickLocationHandler}
+				>
+					{marker}
+				</MapView>
 				<View style={styles.button}>
 					<Button
 						title="Pick Location"
