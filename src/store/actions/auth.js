a0f1a4bfbaacecd0c43737c6e458dc79389d2739
Jacob Bernstein <jacobjbernstein@gmail.com>
2018-12-28 09:54:54 +0900

    Sending info to Firebase for Auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,39 @@
 import { TRY_AUTH } from "./actionTypes";
+import { uiStartLoading, uiStopLoading } from "./index";
 
 export const tryAuth = authData => {
-	return {
-		type: TRY_AUTH,
-		authData: authData
+	return dispatch => {
+		dispatch(authSignup(authData));
+	};
+};
+const key = "AIzaSyD4OKNEEQOk7hHzwevCaNrNLIBWcX12HUs";
+export const authSignup = authData => {
+	return dispatch => {
+		//dispatch(uiStartLoading());
+		fetch(
+			"https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" +
+				key,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					email: authData.email,
+					password: authData.password,
+					returnSecureToken: true
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			//only catches network errors
+			.catch(err => {
+				console.log(err);
+				//dispatch(uiStopLoading());
+				alert("Auth failed, try again.");
+			})
+			.then(res => res.json())
+			.then(parsedRes => {
+				console.log(parsedRes);
+			});
 	};
 };
