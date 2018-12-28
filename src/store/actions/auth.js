31aa0c1831a46bbbf33c06e4c90f70a0daea4f2e
Jacob Bernstein <jacobjbernstein@gmail.com>
2018-12-28 10:25:08 +0900

    Refactor Auth Code

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -4,70 +4,29 @@ import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
 export const tryAuth = (authData, authMode) => {
 	return dispatch => {
-		if (authMode === "login") {
-			dispatch(authLogin(authData));
-		} else {
-			dispatch(authSignup(authData));
-		}
-	};
-};
-const key = "AIzaSyD4OKNEEQOk7hHzwevCaNrNLIBWcX12HUs";
-
-export const authSignup = authData => {
-	return dispatch => {
+		const apiKey = "AIzaSyD4OKNEEQOk7hHzwevCaNrNLIBWcX12HUs";
 		dispatch(uiStartLoading());
-		fetch(
-			"https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" +
-				key,
-			{
-				method: "POST",
-				body: JSON.stringify({
-					email: authData.email,
-					password: authData.password,
-					returnSecureToken: true
-				}),
-				headers: {
-					"Content-Type": "application/json"
-				}
-			}
-		)
-			//only catches network errors
-			.catch(err => {
-				console.log(err);
-				dispatch(uiStopLoading());
-				alert("Auth failed, try again.");
-			})
-			.then(res => res.json())
-			.then(parsedRes => {
-				dispatch(uiStopLoading());
-				if (parsedRes.error) {
-					//only exists if have error
-					alert("There's an error, please try again");
-				} else {
-					startMainTabs();
-				}
-			});
-	};
-};
 
-export const authLogin = authData => {
-	return dispatch => {
-		dispatch(uiStartLoading());
-		fetch(
+		//default URL to login
+		let url =
 			"https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" +
-				key,
-			{
-				method: "POST",
-				body: JSON.stringify({
-					email: authData.email,
-					password: authData.password,
-					returnSecureToken: true
-				}),
-				headers: {
-					"Content-Type": "application/json"
-				}
+			apiKey;
+		if (authMode === "signup") {
+			url =
+				"https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=" +
+				apiKey;
+		}
+		fetch(url, {
+			method: "POST",
+			body: JSON.stringify({
+				email: authData.email,
+				password: authData.password,
+				returnSecureToken: true
+			}),
+			headers: {
+				"Content-Type": "application/json"
 			}
-		)
+		})
 			//only catches network errors
 			.catch(err => {
 				console.log(err);
