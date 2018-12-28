502ea4618cbbcd1b631cdadcdf231dc1c48b0a51
Jacob Bernstein <jacobjbernstein@gmail.com>
2018-12-28 10:12:09 +0900

    Connecting login to firebase auth

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -5,7 +5,7 @@ import startMainTabs from "../../screens/MainTabs/startMainTabs";
 export const tryAuth = (authData, authMode) => {
 	return dispatch => {
 		if (authMode === "login") {
-			//	dispatch(authLogin(authData))
+			dispatch(authLogin(authData));
 		} else {
 			dispatch(authSignup(authData));
 		}
@@ -42,6 +42,44 @@ export const authSignup = authData => {
 				dispatch(uiStopLoading());
 				if (parsedRes.error) {
 					//only exists if have error
+					alert("There's an error, please try again");
+				} else {
+					startMainTabs();
+				}
+			});
+	};
+};
+
+export const authLogin = authData => {
+	return dispatch => {
+		dispatch(uiStartLoading());
+		fetch(
+			"https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=" +
+				key,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					email: authData.email,
+					password: authData.password,
+					returnSecureToken: true
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		)
+			//only catches network errors
+			.catch(err => {
+				console.log(err);
+				dispatch(uiStopLoading());
+				alert("Auth failed, try again.");
+			})
+			.then(res => res.json())
+			.then(parsedRes => {
+				dispatch(uiStopLoading());
+				if (parsedRes.error) {
+					//only exists if have error
+					console.log(parsedRes.error);
 					alert("There's an error, please try again");
 				} else {
 					startMainTabs();
